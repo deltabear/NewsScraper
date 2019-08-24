675622b2bc16f377de580dcc0431694e40b0b204
max delzer <emma.delzer@gmail.com>
2019-08-23 21:15:31 -0700

    Require express-handlebars package

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Require dependencies
 var express = require("express");
 var logger = require("morgan");
 var mongoose = require("mongoose");
@@ -20,84 +21,89 @@ app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 
+//
+var exphbs = require("express-handlebars");
+app.engine("handlebars",exphbs({defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/populatedb", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost/scraped_news", { useNewUrlParser: true });
 
-// When the server starts, create and save a new User document to the db
-// The "unique" rule in the User model's schema will prevent duplicate users from being added to the server
-db.User.create({ name: "Ernest Hemingway" })
-  .then(function(dbUser) {
-    console.log(dbUser);
-  })
-  .catch(function(err) {
-    console.log(err.message);
-  });
+// // When the server starts, create and save a new User document to the db
+// // The "unique" rule in the User model's schema will prevent duplicate users from being added to the server
+// db.User.create({ name: "Ernest Hemingway" })
+//   .then(function(dbUser) {
+//     console.log(dbUser);
+//   })
+//   .catch(function(err) {
+//     console.log(err.message);
+//   });
 
-// Routes
+// // Routes
 
-// Route for retrieving all Notes from the db
-app.get("/notes", function(req, res) {
-  // Find all Notes
-  db.Note.find({})
-    .then(function(dbNote) {
-      // If all Notes are successfully found, send them back to the client
-      res.json(dbNote);
-    })
-    .catch(function(err) {
-      // If an error occurs, send the error back to the client
-      res.json(err);
-    });
-});
+// // Route for retrieving all Notes from the db
+// app.get("/notes", function(req, res) {
+//   // Find all Notes
+//   db.Note.find({})
+//     .then(function(dbNote) {
+//       // If all Notes are successfully found, send them back to the client
+//       res.json(dbNote);
+//     })
+//     .catch(function(err) {
+//       // If an error occurs, send the error back to the client
+//       res.json(err);
+//     });
+// });
 
-// Route for retrieving all Users from the db
-app.get("/user", function(req, res) {
-  // Find all Users
-  db.User.find({})
-    .then(function(dbUser) {
-      // If all Users are successfully found, send them back to the client
-      res.json(dbUser);
-    })
-    .catch(function(err) {
-      // If an error occurs, send the error back to the client
-      res.json(err);
-    });
-});
+// // Route for retrieving all Users from the db
+// app.get("/user", function(req, res) {
+//   // Find all Users
+//   db.User.find({})
+//     .then(function(dbUser) {
+//       // If all Users are successfully found, send them back to the client
+//       res.json(dbUser);
+//     })
+//     .catch(function(err) {
+//       // If an error occurs, send the error back to the client
+//       res.json(err);
+//     });
+// });
 
-// Route for saving a new Note to the db and associating it with a User
-app.post("/submit", function(req, res) {
-  // Create a new Note in the db
-  db.Note.create(req.body)
-    .then(function(dbNote) {
-      // If a Note was created successfully, find one User (there's only one) and push the new Note's _id to the User's `notes` array
-      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-      return db.User.findOneAndUpdate({}, { $push: { notes: dbNote._id } }, { new: true });
-    })
-    .then(function(dbUser) {
-      // If the User was updated successfully, send it back to the client
-      res.json(dbUser);
-    })
-    .catch(function(err) {
-      // If an error occurs, send it back to the client
-      res.json(err);
-    });
-});
+// // Route for saving a new Note to the db and associating it with a User
+// app.post("/submit", function(req, res) {
+//   // Create a new Note in the db
+//   db.Note.create(req.body)
+//     .then(function(dbNote) {
+//       // If a Note was created successfully, find one User (there's only one) and push the new Note's _id to the User's `notes` array
+//       // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+//       // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+//       return db.User.findOneAndUpdate({}, { $push: { notes: dbNote._id } }, { new: true });
+//     })
+//     .then(function(dbUser) {
+//       // If the User was updated successfully, send it back to the client
+//       res.json(dbUser);
+//     })
+//     .catch(function(err) {
+//       // If an error occurs, send it back to the client
+//       res.json(err);
+//     });
+// });
 
-// Route to get all User's and populate them with their notes
-app.get("/populateduser", function(req, res) {
-  // Find all users
-  db.User.find({})
-    // Specify that we want to populate the retrieved users with any associated notes
-    .populate("notes")
-    .then(function(dbUser) {
-      // If able to successfully find and associate all Users and Notes, send them back to the client
-      res.json(dbUser);
-    })
-    .catch(function(err) {
-      // If an error occurs, send it back to the client
-      res.json(err);
-    });
-});
+// // Route to get all User's and populate them with their notes
+// app.get("/populateduser", function(req, res) {
+//   // Find all users
+//   db.User.find({})
+//     // Specify that we want to populate the retrieved users with any associated notes
+//     .populate("notes")
+//     .then(function(dbUser) {
+//       // If able to successfully find and associate all Users and Notes, send them back to the client
+//       res.json(dbUser);
+//     })
+//     .catch(function(err) {
+//       // If an error occurs, send it back to the client
+//       res.json(err);
+//     });
+// });
 
 // Start the server
 app.listen(PORT, function() {
